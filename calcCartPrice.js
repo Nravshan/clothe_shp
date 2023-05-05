e949515f50c638c28739dc3736124754afd6922a
Nravshan <ravshanniyozov7@gmail.com>
2023-05-05 14:39:29 +0300

    first commit

---
+++ calcCartPrice.js
@@ -0,0 +1,9 @@
+function calcCartPrice () {
+     const gridbox = document.querySelectorAll('.products grid')
+     console.log(gridbox);
+
+     gridbox.forEach(function (item) {
+        console.log(item);
+
+     })
+}
